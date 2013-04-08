64ea01e2a76180466275aedb5924330b4ef1ba2a
Jae Young Ryoo <jay.ryoo@gmail.com>
2013-04-07 22:40:30 -0700

    created scheduler.rake under lib/tasks

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -27,8 +27,27 @@ class FeedEntry < ActiveRecord::Base
   				:published_at	=> entry.published,
   				:guid 			=> entry.id
   			)
+        Member.send_RSS(entry)
   		end
-  		Member.send_RSS(entry)
   	end
   end
+
+  def self.add_entry(entry)
+    unless exists? :guid => entry.id
+      create!(
+        :name     => entry.title,
+        :summary    => entry.summary,
+        :url      => entry.url,
+        :published_at => entry.published,
+        :guid       => entry.id
+      )
+    end
+    Member.send_RSS(entry)
+  end
+
+  def self.update_from_feed_once_testing(feed_url)
+    feed = Feedzirra::Feed.fetch_and_parse(feed_url)
+    add_entry(feed.entries[0])
+  end
+
 end
---
+++ lib/tasks/scheduler.rake
@@ -0,0 +1,10 @@
+desc "This task is called by the Heroku scheduler add-on"
+task :update_feed => :environment do
+  puts "Updating feed..."
+  FeedEntry.update_from_feed_once_testing("http://www.swbno.org/rss.asp?feed=pr")
+  puts "done."
+end
+
+#task :send_reminders => :environment do
+#  User.send_reminders
+#end

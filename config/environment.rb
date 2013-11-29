e21f09ee3911c786edcd08536dbf033e23e3c4cc
jryoo <jay.ryoo@gmail.com>
2013-11-28 23:47:50 -0800

    env vars

--- config/environment.rb
+++ config/environment.rb
@@ -9,15 +9,8 @@ Mail.defaults do
   delivery_method :smtp, { :address   => "smtp.gmail.com",
                            :port      => 587,
                            :domain    => "google.com",
-                           :user_name => "nolaswnotice@gmail.com",
+                           :user_name => "notifybitcoin@gmail.com",
                            :password  => ENV['EMAIL_PASSWORD'],
                            :authentication => 'plain',
                            :enable_starttls_auto => true }
-end
-
-Twitter.configure do |config|
-  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-  config.consumer_secret = 	ENV['TWITTER_CONSUMER_SECRET']
-  config.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
-  config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
 end
